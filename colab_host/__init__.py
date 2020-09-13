2343f55d1b0d6c5fa074bf40346b1421c3fcf13c
Puneetha Pai <puneethapai29@gmail.com>
2020-09-13 16:32:33 +0530

    Bump version: 0.1.5 → 0.1.6

--- colab_host/__init__.py
+++ colab_host/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Puneetha Pai"""
 __email__ = "puneethapai29@gmail.com"
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 from .colab_host import Host, SimpleHttpServer, JupyterNotebook, JupyterLab
