2efb81fb24671977e67d5f8de9990ff412bf88a3
Puneetha Pai <puneethapai29@gmail.com>
2020-09-13 11:33:17 +0530

    Bump version: 0.1.2 → 0.1.3

--- colab_host/__init__.py
+++ colab_host/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Puneetha Pai"""
 __email__ = "puneethapai29@gmail.com"
-__version__ = "__version__ = "0.1.2"'"
+__version__ = "0.1.3"
 
 from .colab_host import Host, SimpleHttpServer
